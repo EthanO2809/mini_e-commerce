2304f822d0c72894bb2d674282f80a401fa28565
Ethan Oliver <ethanliamoliver28@gmail.com>
2023-08-17 12:32:51 +0200

    backend

--- api/controller/index.js
+++ api/controller/index.js
@@ -1,51 +1,92 @@
-//controller
-const express = require("express");
-const bodyParser = require("body-parser");
-const routes = express.Router();
+const express = require('express')
+const bodyParser = require('body-parser')
+const {verifyAToken} = require('../middleware/AuthenticateUser')
+const routes = express.Router()
 
 //import all model's objects
-const { users } = require("../model");
-const { Products } = require("../model");
+const {users, books, bookauthor, orders} = require('../model')
 
-//========== user's router ============
-routes.get("/users", (req, res) => {
-  users.fetchUsers(req, res);
-});
-
-routes.get("user/:id", (req, res) => {
-  users.fetchUsers(req, res);
-});
-
-routes.post("/register", bodyParser.json(), (req, res) => {
-  users.register(req, res);
-});
-
-routes.post("/login", bodyParser.json(), (req, res) => {
-  users.login(req, res);
-});
-routes.delete("/user/:id", (req, res) => {
-  users.removeUser(req, res);
-});
-routes.patch("/user/:id", bodyParser.json(), (req, res) => {
-  users.updateUser(req, res);
-});
-
-//========== book's router ============
-routes.get("/Products", (req, res) => {
-  Products.fetchProducts(req, res);
-});
-routes.get("/Products/:ProductID", (req, res) => {
-  Products.fetchProduct(req, res);
-});
-routes.post("/Products", bodyParser.json(), (req, res) => {
-  Products.addProduct(req, res);
-});
-routes.patch("/Products/:ProductID", bodyParser.json(), (req, res) => {
-  Products.updateBook(req, res);
-});
-routes.delete("/Products/:ProductID", (req, res)=>{
-    Products.removeProduct(req, res)
+//=======user's router=========
+routes.get('/users',(req, res) => {
+    users.fetchUsers(req, res)
 })
 
-//========== end of routers ============
-module.exports = { express, routes };
+routes.get('/user/:id', (req, res) => {
+    users.fetchUsers(req, res)
+})
+
+routes.post('/register', bodyParser.json(), (req, res) => {
+    users.register(req, res)
+})
+
+routes.put('/user/:id', bodyParser.json(), (req, res) => {
+    users.updateUser(req, res)
+})
+
+routes.patch('/user/:id', bodyParser.json(), (req, res) => {
+    users.updateUser(req, res)
+})
+
+routes.delete('/user/:id', (req, res) => {
+    users.deleteUser(req, res)
+})
+
+routes.post('/login',
+bodyParser.json(), (req, res)=>{
+    users.login(req, res)
+})
+
+routes.get("/books", (req, res) => {
+    books.fetchBooks(req, res);
+  });
+  routes.get("/book/:id", (req, res) => {
+    books.fetchBook(req, res);
+  });
+  routes.post("/newbook", bodyParser.json(), (req, res) => {
+    books.addBook(req, res);
+  });
+  routes.patch("/book/:id", bodyParser.json(), (req, res) => {
+    books.updateBook(req, res);
+  });
+  routes.delete("/book/:id", (req, res) => {
+    books.removeBook(req, res);
+  });
+  // ============ Books' router ==============
+  routes.get("/bookauthors", (req, res) => {
+    bookAuthors.fetchAuthors(req, res);
+  });
+  routes.get("/bookauthor/:id", (req, res) => {
+    bookAuthors.fetchAuthor(req, res);
+  });
+  routes.post("/addauthor", bodyParser.json(), (req, res) => {
+    bookAuthors.addAuthor(req, res);
+  });
+  routes.patch("/bookauthor/:id", bodyParser.json(), (req, res) => {
+    bookAuthors.updateAuthor(req, res);
+  });
+  routes.delete("/bookauthor/:id", (req, res) => {
+    bookAuthors.removeAuthor(req, res);
+  });
+
+  // ============ Orders' router ==============
+routes.get("/orders", (req, res) => {
+  orders.fetchOrders(req, res);
+});
+routes.get("/order/:id", (req, res) => {
+  orders.fetchOrder(req, res);
+});
+routes.post("/addOrder/:orderID//:userID/:bookID", bodyParser.json(), (req, res)=>{
+  orders.insertOrder(req, res)
+})
+routes.put("/order/:orderID", bodyParser.json(), (req, res) => {
+  orders.updateOrder(req, res);
+});
+routes.delete("/order/:orderID", (req, res)=>{
+  orders.removeOrder(req, res)
+})
+
+module.exports = {
+    express,
+    routes,
+    verifyAToken
+}
--- api/index.js
+++ api/index.js
@@ -1,15 +1,49 @@
+const cookieParser = require("cookie-parser");
 const { express, routes } = require("./controller");
 const path = require("path");
-const port = +process.env.PORT || 3017;
+// const log = require('console');
+const errorHandling = require('./middleware/ErrorHandling')
+const port = +process.env.PORT || 3000;
 const app = express();
+const cors = require("cors");
+// log("")
 
-app.use(express.static("./static"));
+app.use(express.static('./static'),
+  express.urlencoded({ 
+    extended: false 
+  }),
+  cookieParser(),
+  cors(),
+  routes
+  );
 
-app.use(express.urlencoded({ extended: false }), routes);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+})
 
-routes.get("^/$|/miniEcom", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./static/html/index.html"));
-});
+routes.get("^/$|/mini e-com", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./static/html/index.html"));
+  }
+);
+
+// cookieParser & Router
+// cookieParser should be set before router
+app.use(cookieParser(), cors(), routes);
+app.use(
+  express.json(),
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// Handling all errors
+app.use(errorHandling);
 
 app.listen(port, () => {
   console.log(`You are listening on port ${port}`);
